f055500b280d9a84aadc553bb155f13d4902f95b
nandareddy431 <nandareddy375@gmail.com>
2020-07-08 00:08:07 +0530

    This my latest login function

---
+++ src/main/java/com/qa/hubspot/pages/LoginPage2.java
@@ -0,0 +1,51 @@
+package com.qa.hubspot.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.qa.hubspot.basepage.BasePage;
+
+public class LoginPage2 extends BasePage {
+	WebDriver driver;
+	// 1. Page Objects / By Locators
+	By emailID = By.id("username");
+	By password = By.id("password");
+	By loginButton = By.id("loginBtn");
+	By signUpLink = By.linkText("Sign up");
+
+	// 2. page class constructor:
+	public LoginPage2(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public String getLoginPageTitle() {
+		return driver.getTitle();
+	}
+     
+	
+	
+	
+	public boolean isSignUpDisplayed() {
+		System.out.println("nanda"
+				                        + "reddy");
+		
+		return driver.findElement(signUpLink).isDisplayed();
+		
+	}
+
+	public HomePage doLogin(String username, String pwd) {
+		driver.findElement(emailID).sendKeys(username);
+		driver.findElement(password).sendKeys(pwd);
+		driver.findElement(loginButton).click();
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return new HomePage(driver);
+	}
+	
+	
+}
